1fd07c17d21a8a47cd35a8e9b27c6d686daefadc
David Flemström <dflemstr@spotify.com>
2019-02-18 19:06:00 +0100

    Allow graceful exits

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ mod ui;
 enum Event {
     Input(termion::event::Event, bytes::BytesMut),
     Output(usize, bytes::BytesMut),
+    Exit(usize, std::process::ExitStatus),
 }
 
 fn main() {
@@ -38,7 +39,6 @@ fn main() {
 }
 
 fn run() -> Result<(), failure::Error> {
-    use futures::future::Future;
     use std::fs;
     use structopt::StructOpt;
 
@@ -72,31 +72,19 @@ fn run() -> Result<(), failure::Error> {
 
     info!("starting");
 
-    let mut runtime = tokio::runtime::Runtime::new()?;
-    let exit_statuses = runtime.block_on(tokio_async_await::compat::backward::Compat::new(
+    let runtime = tokio::runtime::Runtime::new()?;
+    runtime.block_on_all(tokio_async_await::compat::backward::Compat::new(
         run_with_options(options),
     ))?;
-    debug!("terminating runtime");
-    runtime.shutdown_now().wait().unwrap();
-
-    for (index, exit_status) in exit_statuses {
-        if !exit_status.success() {
-            return Err(failure::err_msg(format!(
-                "process with index {} failed with {}",
-                index, exit_status
-            )));
-        }
-    }
 
     info!("done");
 
     Ok(())
 }
 
-async fn run_with_options(
-    mut options: options::Options,
-) -> Result<Vec<(usize, std::process::ExitStatus)>, failure::Error> {
+async fn run_with_options(mut options: options::Options) -> Result<(), failure::Error> {
     use futures::future::Future;
+    use futures::stream::Stream;
 
     let args = await!(args::read(&mut options))?;
     let command = options.command;
@@ -106,6 +94,8 @@ async fn run_with_options(
         .enumerate()
         .map(move |(index, args)| process::Process::spawn(index, command.clone(), args))
         .collect::<Result<Vec<_>, failure::Error>>()?;
+
+    debug!("spawned {} processes", processes.len());
 
     let inputs = processes
         .iter_mut()
@@ -117,7 +107,10 @@ async fn run_with_options(
         .map(|p| p.output.take().unwrap())
         .collect::<Vec<_>>();
 
-    debug!("spawned {} processes", processes.len());
+    let children = processes
+        .into_iter()
+        .map(|p| p.child.map_err(failure::Error::from))
+        .collect::<Vec<_>>();
 
     let mut tty_output = tty::Tty::open()?.into_raw_mode()?;
     let tty_input = tty_output.try_clone()?;
@@ -130,34 +123,22 @@ async fn run_with_options(
     debug!("created terminal");
 
     let events = read_events(tty_input);
+    let input = run_gui(outputs, children, terminal, events)?;
 
-    let stdin = run_gui(outputs, terminal, events)?;
+    let rest = await!(forward_stdin(inputs, input))?;
 
-    tokio::spawn_async(
-        async {
-            await!(forward_stdin(inputs, stdin))
-                .unwrap_or_else(|err| error!("failed to forward stdin: {:?}", err))
-        },
-    );
+    debug!("stdin closed, discarding further input");
 
-    debug!("waiting for processes to finish");
+    await!(rest.into_future().map_err(|(e, _)| e))?;
 
-    let exit_statuses_future = futures::future::join_all(processes.into_iter().map(|p| {
-        let i = p.index;
-        p.child
-            .inspect(move |x| debug!("process {} exited with {}", i, x))
-            .map(move |x| (i, x))
-    }));
-
-    let exit_statuses = await!(exit_statuses_future)?;
-
-    debug!("all processes finished");
-
-    Ok(exit_statuses)
+    Ok(())
 }
 
 fn run_gui(
     outputs: Vec<impl futures::stream::Stream<Item = bytes::BytesMut, Error = failure::Error>>,
+    exits: Vec<
+        impl futures::future::Future<Item = std::process::ExitStatus, Error = failure::Error>,
+    >,
     mut terminal: tui::Terminal<impl tui::backend::Backend + 'static>,
     events: impl futures::stream::Stream<Item = Event, Error = failure::Error>,
 ) -> Result<impl futures::Stream<Item = bytes::BytesMut, Error = failure::Error>, failure::Error> {
@@ -174,14 +155,25 @@ fn run_gui(
             .map(|(i, o)| o.map(move |b| Event::Output(i, b))),
     );
 
+    let exit = futures::stream::futures_unordered(
+        exits
+            .into_iter()
+            .enumerate()
+            .map(|(i, e)| e.map(move |e| Event::Exit(i, e))),
+    );
+
     terminal.draw(|f| render_ui(&state, num_outputs, f))?;
 
     let output = events
         .select(output)
+        .select(exit)
         .and_then(move |event| {
             match &event {
                 Event::Output(idx, data) => {
                     state[*idx].push_str(str::from_utf8(&data)?);
+                }
+                Event::Exit(idx, status) => {
+                    state[*idx].push_str(&format!("\nprocess {} exited with {}", idx, status));
                 }
                 _ => {}
             };
@@ -216,19 +208,13 @@ fn render_ui(
         .split(f.size());
 
     for (i, output) in state.iter().enumerate() {
-        tui::widgets::Paragraph::new(
-            output
-                .lines()
-                .map(|line| tui::widgets::Text::Raw(line.into()))
-                .collect::<Vec<_>>()
-                .iter(),
-        )
-        .block(
-            tui::widgets::Block::default()
-                .borders(tui::widgets::Borders::ALL)
-                .title(&format!("{}", i)),
-        )
-        .render(&mut f, chunks[i]);
+        tui::widgets::Paragraph::new([tui::widgets::Text::Raw(output.into())].iter())
+            .block(
+                tui::widgets::Block::default()
+                    .borders(tui::widgets::Borders::ALL)
+                    .title(&format!("{}", i)),
+            )
+            .render(&mut f, chunks[i]);
     }
 }
 
@@ -241,7 +227,11 @@ fn read_events(
     let event_iterator = read.events_and_raw();
 
     let raw_events_stream =
-        stream_utils::blocking_iter_to_stream(event_iterator).map_err(failure::Error::from);
+        stream_utils::blocking_iter_to_stream(event_iterator.take_while(|e| match e {
+            Ok((termion::event::Event::Key(termion::event::Key::Ctrl('t')), _)) => false,
+            _ => true,
+        }))
+        .map_err(failure::Error::from);
 
     raw_events_stream.and_then(move |event| match event? {
         (event, data) => Ok(Event::Input(event, data.into())),
@@ -253,14 +243,17 @@ async fn forward_stdin(
         impl futures::sink::Sink<SinkItem = bytes::Bytes, SinkError = failure::Error> + Send + 'static,
     >,
     stdin: impl futures::stream::Stream<Item = bytes::BytesMut, Error = failure::Error> + Send + 'static,
-) -> Result<(), failure::Error> {
+) -> Result<
+    impl futures::stream::Stream<Item = bytes::Bytes, Error = failure::Error> + Send + 'static,
+    failure::Error,
+> {
     use futures::stream::Stream;
 
-    await!(stdin
+    let (rest, _) = await!(stdin
         .map(bytes::BytesMut::freeze)
         .forward(fanout::Fanout::new(inputs)))?;
 
-    Ok(())
+    Ok(rest)
 }
 
 async fn create_terminal(
